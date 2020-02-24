a86084b89e54a0af7d177535a606d0d6f195e715
mm-irvingleonard <irving.leonard@modmed.com>
2020-02-24 16:05:53 -0500

    fix(code): toward pythonic ways

--- _modules/snmp.py
+++ _modules/snmp.py
@@ -2,37 +2,45 @@
 '''SNMP agent execution module.
 This module implements the execution actions related to SNMP.
 
-https://stackoverflow.com/questions/6819399/remove-user-in-snmp-by-agent
-
 Version: 0.0.1
 
 TODO:
 - everything
 
 Refs:
+- https://stackoverflow.com/questions/6819399/remove-user-in-snmp-by-agent
 '''
 
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
-def check_user(username):
+def start_snmpd(init_binary, parameters):
+	'''Start the SNMPd deamon
+	Use the init program to start the SNMPd deamon.
+	'''
+	
+	service_start_command = 'systemctl start snmpd.service'
+	command_str = __salt__['cmd.run'](service_stop_command)
+	
+def stop_snmpd(init_binary, parameters):
+	'''Stop the SNMPd deamon
+	Use the init program to stop the SNMPd deamon.
+	'''
+	
+	service_stop_command = 'systemctl stop snmpd.service'
+	return command_str = __salt__['cmd.run'](service_stop_command)
+
+def check_user(username, snmpd_conf_path = '/etc/snmp/snmpd.conf'):
 	'''Check user list
 	Finds a user in the list of registered users. Returns true if is in the list, false otherwise.
 	'''
-	with open ('/etc/snmp/snmpd.conf', 'r') as f:
-		f_content = f.read()
-        #print(f_content)
-        value = False
-        for  line in f_content:
-            if username in line:
-                value = True
-                break;
-            else:
-                value = False
-        return value
-
-# 	command_str = __salt__['cmd.run']('ls -l')
+	
+	with open(snmpd_conf_path, 'r') as f:
+		for	line in f.readlines():
+			if username in line:
+				return True
+		return False
 
 def add_user(username, authpass, privpass, read_only = False, auth_hash_sha = True, encryption_aes = True):
 	'''Add user
@@ -45,27 +53,29 @@ def add_user(username, authpass, privpass, read_only = False, auth_hash_sha = Tr
 	parameters += ['-a', 'SHA' if auth_hash_sha else 'MD5']
 	parameters += ['-X', privpass]
 	parameters += ['-x', 'AES' if encryption_aes else 'DES']
-    parameters += [username]
+	parameters += [username]
 
 	command_str = __salt__['cmd.run']('net-snmp-create-v3-user ' + ' '.join(parameters))
 
-def del_user(username):
+def del_user(username, snmpd_conf_path = '/etc/snmp/snmpd.conf', snmpd_conf_var_path = '/var/lib/net-snmp/snmpd.conf'):
 	'''Delete user
 	Removes a user from the user list. Returns None.
 	'''
-	command_str = __salt__['cmd.run']('systemctl ' + 'stop ' + 'snmpd.service')
+	
+	stop_snmpd()
 
-	with open ('/etc/snmp/snmpd.conf', 'r') as f:
-        etc_lines = f.readlines()
-	with open ('/var/lib/net-snmp/snmpd.conf', 'r') as f1:
-        var_lines = f1.readlines()
+	with open (snmpd_conf_path, 'r') as f:
+		etc_lines = f.readlines()
+	with open (snmpd_conf_var_path, 'r') as f1:
+		var_lines = f1.readlines()
 
-    with open ('/etc/snmp/snmpd.conf', 'w') as f:
-        for line in etc_lines:
-            if username not in line.strip("\n"):
-                f.write(line)
-	with open ('/var/lib/net-snmp/snmpd.conf', 'w') as f1:
-        for line in var_lines:
-            if 'usmUser' not in line.strip("\n"):
-                f1.write(line)
-	command_str = __salt__['cmd.run']('systemctl ' + 'start ' + 'snmpd.service')
+	with open (snmpd_conf_path, 'w') as f:
+		for line in etc_lines:
+			if username not in line.strip("\n"):
+				f.write(line)
+	with open (snmpd_conf_var_path, 'w') as f1:
+		for line in var_lines:
+			if 'usmUser' not in line.strip("\n"):
+				f1.write(line)
+	
+	start_snmpd()
